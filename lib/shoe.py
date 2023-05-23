713213988ccb4838983e73a14d109accfd3d7208
dennis-nganga <denniskimani416@gmail.com>
2023-05-23 21:51:15 +0300

    putting together

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=0):
+        self.brand = brand
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
